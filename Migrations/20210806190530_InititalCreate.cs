0711d0d74e7a205e9b24051d8251783d6edfe865
SAMUEL REYES <samuelreyesiglesias@gmail.com>
2021-08-06 13:36:27 -0600

    Add project files.

---
+++ Migrations/20210806190530_InititalCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EXAMPLEEFCF.Migrations
+{
+    public partial class InititalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VersionOfMe",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VersionOfMe", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarGranTrofeo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModelCar = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VersionOfMeid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarGranTrofeo", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_CarGranTrofeo_VersionOfMe_VersionOfMeid",
+                        column: x => x.VersionOfMeid,
+                        principalTable: "VersionOfMe",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarGranTrofeo_VersionOfMeid",
+                table: "CarGranTrofeo",
+                column: "VersionOfMeid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarGranTrofeo");
+
+            migrationBuilder.DropTable(
+                name: "VersionOfMe");
+        }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace EXAMPLEEFCF.Models
+{
+    public class MyDbContext: DbContext 
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+         public   DbSet<VersionOfMe> VersionOfMe { get; set; }
+        public DbSet<CarGranTrofeo> CarGranTrofeo { get; set; }
+    }
+
+    public class VersionOfMe
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public ICollection<CarGranTrofeo> CarGranTrofeos { get; set; }
+    }
+    public class CarGranTrofeo
+    {
+        public int id { get; set; }
+        public string ModelCar { get; set; }
+        //public int VersionOfMeId { get; set; }
+        //public VersionOfMe VersionOfMe { get; set; }
+    }
+}
